ddffcdab425c05d91c6cd96ff947f4d775d5c06f
liwen <burning-life@sjtu.edu.cn>
2018-06-07 13:32:54 +0800

    change 1

--- crawler/crawler2.py
+++ crawler/crawler2.py
@@ -3,18 +3,15 @@ from pyquery import PyQuery
 import sqlite3
 import urllib3
 import re
+import os
 
 
 class BookKm(object):
     def __init__(self):
         super().__init__()
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36'}
-        #self.webkit = webdriver.Chrome()
+        self.webkit = None
         self.url_base = 'http://book.km.com'
-        self.classes = {1: '玄幻', 2: '奇幻', 3: '武侠', 4: '仙侠', 5: '都市', 6: 'None', 7: '军事', 8: '历史', 9: '游戏',
-                        10: '科幻', 11: '现代言情', 12: '古代言情', 13: '穿越架空', 14: '总裁豪门', 15: '青春校园', 16: '同人',
-                        17: '灵异', 18: '悬疑', 19: '文学', 20: '职场', 21: '其他', 22: '黑道', 23: '官场', 24: '女尊',
-                        25: '种田', 26: '异界', 27: '流行小说'}
         self.classes_num = 37
         self.http = urllib3.PoolManager()
         self.db_conn = None
@@ -42,6 +39,7 @@ class BookKm(object):
         try:
             cursor = self.db_conn.cursor()
         except:
+            print('connect to database...')
             self.db_conn = sqlite3.connect('bk.db')
             cursor = self.db_conn.cursor()
         try:
@@ -75,6 +73,9 @@ class BookKm(object):
     def make_page_url(self, class_id, page_id):
         return self.url_base + '/shuku_0_' + str(class_id) + '_0_0_0_0_' + str(page_id) + '.html'
 
+    def make_book_url(self, bid):
+        return self.url_base + '/chapterlist/' + str(bid) + '.html'
+
     def get_page_num_and_class_name(self, url):
         reply = self.http.request('GET', url, self.headers)
         data = reply.data.decode('GBK')
@@ -89,7 +90,7 @@ class BookKm(object):
         return [page_num, class_name]
 
     def get_all_books(self):
-        for class_id in range(1, 1 + self.classes_num):
+        for class_id in range(27, 1 + self.classes_num):
             page_num, class_name = self.get_page_num_and_class_name(self.make_page_url(class_id, 1))
             for page_id in range(1, 1 + page_num):
                 self.get_books_on_page(self.make_page_url(class_id, page_id), class_name)
@@ -104,7 +105,59 @@ class BookKm(object):
         self.get_all_books()
         self.finish()
 
+    def download_contents(self):
+        try:
+            self.webkit = webdriver.Chrome()
+            self.webkit.implicitly_wait(3)
+        except:
+            print('Chrome-driver not found, exit')
+            exit()
+        try:
+            cursor = self.db_conn.cursor()
+        except:
+            print('connect to database...')
+            self.db_conn = sqlite3.connect('bk.db')
+            cursor = self.db_conn.cursor()
+        cursor.execute('SELECT * FROM BkBooks')
+        book_list = cursor.fetchall()
+        for book in book_list:
+            bid = book[0]
+            print('[Downloading book: %s]' % book[1])
+            self.download_book(bid)
+        self.webkit.close()
+
+    def default_content(self, bid):
+        if not os.path.exists('./books/' + str(bid)):
+            os.mkdir('./books/' + str(bid))
+        with open('./books/' + str(bid) + '/1.txt', 'w') as f:
+            f.write("小说不支持预览")
+
+    def content(self, bid, cid, url):
+        if not os.path.exists('./books/' + str(bid)):
+            os.mkdir('./books/' + str(bid))
+        with open('./books/' + str(bid) + '/' + str(cid) + '.txt', 'w') as f:
+            self.webkit.get(url)
+            text = self.webkit.find_element_by_class_name('article-body').text
+            f.write(text)
+
+    def download_book(self, bid):
+        try:
+            reply = self.http.request('GET', self.make_book_url(bid), self.headers)
+            data = reply.data.decode('GBK')
+            jq = PyQuery(data)
+            chapter_list = jq('.mod_catalog')('li')
+            for cid in range(1, chapter_list.length + 1):
+                if chapter_list.eq(cid - 1).attr('class') is not None:
+                    break
+                chapter = chapter_list.eq(cid - 1)('a').eq(0).attr('href')
+                self.content(bid, cid, self.url_base + chapter)
+            print('Successful')
+        except:
+            self.default_content(bid)
+            print('Failed')
+
 
 if __name__ == '__main__':
     crawler = BookKm()
-    crawler.run()
+    #crawler.run()
+    crawler.download_contents()
